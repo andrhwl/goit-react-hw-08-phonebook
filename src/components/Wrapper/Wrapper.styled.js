1989af0c6678484828e52161a13c86c78d814b87
andrhwl <120339957+andrhwl@users.noreply.github.com>
2023-10-01 21:45:04 +0300

    ad

--- src/components/Wrapper/Wrapper.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 0 auto;
-  text-align: center;
-  max-width: 760px;
-`;
